028ff6f09f43e1adb68f8ae8bcfcfad0ac3bfb5f
DUSAN DULKA <Mardaquat@protonmail.com>
2022-10-17 15:15:11 +0200

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar">
+            <div className='navbar-container'>
+                <link to="/" className='navbar-logo'>
+                    TRVL
+                </link>
+            </div>
+        </nav>
+   </>
+  )
+}
+
+export default Navbar
